ff51d79cd5ab69c346072b9e129b26c3943df981
hthg13 <32647181+hthg13@users.noreply.github.com>
2020-03-16 21:22:21 +0000

    login prufur og profile ásamt code review möppu með assignment2

--- app/src/main/java/com/example/client/ui/login/LoginViewModel.java
+++ app/src/main/java/com/example/client/ui/login/LoginViewModel.java
@@ -8,8 +8,8 @@ import android.util.Patterns;
 
 import com.example.client.data.LoginRepository;
 import com.example.client.data.Result;
-import com.example.client.data.entities.LoggedInUser;
 import com.example.client.R;
+import com.example.client.data.entities.User;
 
 public class LoginViewModel extends ViewModel {
 
@@ -31,11 +31,11 @@ public class LoginViewModel extends ViewModel {
 
     public void login(String username, String password) {
         // can be launched in a separate asynchronous job
-        Result<LoggedInUser> result = loginRepository.login(username, password);
+        Result<User> result = loginRepository.login(username, password);
 
         if (result instanceof Result.Success) {
-            LoggedInUser data = ((Result.Success<LoggedInUser>) result).getData();
-            loginResult.setValue(new LoginResult(new LoggedInUserView(data.getDisplayName())));
+            User data = ((Result.Success<User>) result).getData();
+            loginResult.setValue(new LoginResult(new LoggedInUserView(data.getUserName())));
         } else {
             loginResult.setValue(new LoginResult(R.string.login_failed));
         }
